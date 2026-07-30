0208d8e84981dc25f7c6be0d9f88c3282f4735b0
Bat_z7 <waleedbatayyah@gmail.com>
2026-07-30 05:42:26 +0300

    Implement 3D rotation effect on card
    Add mouse movement effect to card element

--- css/css/css/css/js/script.js
+++ css/css/css/css/js/script.js
@@ -1,1 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const card = document.querySelector(".card");
+
+    document.addEventListener("mousemove", (e) => {
+
+        const x = (window.innerWidth / 2 - e.clientX) / 35;
+        const y = (window.innerHeight / 2 - e.clientY) / 35;
+
+        card.style.transform =
+            `rotateY(${-x}deg) rotateX(${y}deg)`;
+
+    });
+
+    document.addEventListener("mouseleave", () => {
+
+        card.style.transform =
+            "rotateY(0deg) rotateX(0deg)";
+
+    });
+
+});
