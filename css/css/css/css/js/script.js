879d5710d17ba0556df68b9b7e300807a4878e4d
Bat_z7 <waleedbatayyah@gmail.com>
2026-07-30 06:13:16 +0300

    Update script.js

--- css/css/css/css/js/script.js
+++ css/css/css/css/js/script.js
@@ -1,34 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const card = document.querySelector(".glass-card");
-    const glow = document.querySelector(".cursor-glow");
 
-    if (card) {
-        document.addEventListener("mousemove", (e) => {
+    if (!card) return;
 
-            const x = (window.innerWidth / 2 - e.clientX) / 35;
-            const y = (window.innerHeight / 2 - e.clientY) / 35;
+    document.addEventListener("mousemove", (e) => {
 
-            card.style.transform =
-                `rotateY(${-x}deg) rotateX(${y}deg)`;
+        const x = (window.innerWidth / 2 - e.clientX) / 35;
+        const y = (window.innerHeight / 2 - e.clientY) / 35;
 
-        });
+        card.style.transform =
+            `rotateY(${-x}deg) rotateX(${y}deg)`;
 
-        document.addEventListener("mouseleave", () => {
+    });
 
-            card.style.transform =
-                "rotateY(0deg) rotateX(0deg)";
+    document.addEventListener("mouseleave", () => {
 
-        });
-    }
+        card.style.transform =
+            "rotateY(0deg) rotateX(0deg)";
 
-    if (glow) {
-        document.addEventListener("mousemove", (e) => {
-
-            glow.style.left = e.clientX + "px";
-            glow.style.top = e.clientY + "px";
-
-        });
-    }
+    });
 
 });
