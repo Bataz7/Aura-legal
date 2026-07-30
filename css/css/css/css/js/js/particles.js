b79160f0f4f289e7364cfb3d02e286eda6435820
Bat_z7 <waleedbatayyah@gmail.com>
2026-07-30 05:43:03 +0300

    Update particles.js

--- css/css/css/css/js/js/particles.js
+++ css/css/css/css/js/js/particles.js
@@ -1,1 +1,20 @@
+const bg = document.querySelector(".bg");
 
+for(let i = 0; i < 25; i++){
+
+    const p = document.createElement("div");
+
+    p.className = "particle";
+
+    p.style.left = Math.random()*100 + "%";
+    p.style.top = Math.random()*100 + "%";
+
+    p.style.animationDuration =
+        6 + Math.random()*10 + "s";
+
+    p.style.animationDelay =
+        Math.random()*5 + "s";
+
+    bg.appendChild(p);
+
+}
