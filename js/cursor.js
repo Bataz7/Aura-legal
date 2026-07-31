50e015e6bfb568c36b8f96befb4ecf89f6d4978b
Bat_z7 <waleedbatayyah@gmail.com>
2026-07-31 05:58:30 +0300

    Update cursor.js

--- js/cursor.js
+++ js/cursor.js
@@ -1,8 +1,1 @@
-const glow = document.querySelector(".cursor-glow");
 
-if (glow) {
-    document.addEventListener("mousemove", (e) => {
-        glow.style.left = e.clientX + "px";
-        glow.style.top = e.clientY + "px";
-    });
-}
